38ddcf239c1560063c2e91ac43d42a728eb9c377
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-08-28 23:06:58 +0200

    Add function to generate hashed filename and save downloaded audio file in 'downloads' dir

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import os
-
+import hashlib
 import pytube
+from typing import Tuple
 
 YOUTUBE_VIDEO_URL = "https://www.youtube.com/watch?v=Fv_3IfieHuU&t=11s"
 OUTPUT_PATH = "downloads"
 
 
-def download_audio(url):
+def generate_hashed_path(default_filename: str) -> str:
+    """
+    Use sha256 to hash filename
+    """
+    return hashlib.sha256(default_filename.encode()).hexdigest()
+
+
+def download_audio(url: str) -> Tuple[str, str]:
     """
     Download only audio from youtube video and save in .mp4 format
     """
     yt_video = pytube.YouTube(url)
+    # Filter to download only audio
     audio_stream = yt_video.streams.filter(only_audio=True).first()
-    audio_file_path = audio_stream.download(output_path=OUTPUT_PATH)
 
-    return audio_file_path
+    # Generate hashed directory name and save in downloads
+    directory = generate_hashed_path(audio_stream.default_filename)
+    output_file_path = os.path.join(OUTPUT_PATH, directory)
+
+    audio_file_path = audio_stream.download(output_path=output_file_path)
+    return audio_file_path, directory
 
 
 if __name__ == "__main__":
